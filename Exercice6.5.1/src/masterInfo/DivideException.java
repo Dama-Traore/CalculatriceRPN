0f5d50e8fe972b08d1b33d5bd2cb2477dd4cf0ce
maimouna dama <mayimounadama@gmail.com>
2018-11-13 13:35:31 +0100

    ajout de la classe divide Exception

---
+++ Exercice6.5.1/src/masterInfo/DivideException.java
@@ -0,0 +1,12 @@
+package masterInfo;
+import java.lang.Exception;
+
+public class DivideException extends CalculatriceRPNExceptions {
+	
+	public DivideException()
+    {
+        super();
+    }
+
+
+}
