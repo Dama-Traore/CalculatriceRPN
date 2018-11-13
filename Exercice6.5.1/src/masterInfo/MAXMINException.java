83664b5e773735829dc40906df870754e774d200
maimouna dama <mayimounadama@gmail.com>
2018-11-13 23:03:21 +0100

    Ajout de la classe MAXMINException

---
+++ Exercice6.5.1/src/masterInfo/MAXMINException.java
@@ -0,0 +1,20 @@
+package masterInfo;
+
+public class MAXMINException extends CalculatriceRPNExceptions {
+
+	
+
+	
+	public MAXMINException()
+    {
+        super();
+    }
+
+	
+	
+	 public MAXMINException (String message)
+	    {
+	        super (message);
+	    }
+	 
+}
