3e05051d730dd96ece7973a5ea496f83f58102ac
TRAORE Wilfried Ben Rachid <w_traore@homail.fr>
2018-11-14 01:27:17 +0100

    Implementation de la classe MoteurRPN

--- Exercice6.5.1/src/masterInfo/MoteurRPN.java
+++ Exercice6.5.1/src/masterInfo/MoteurRPN.java
@@ -5,77 +5,51 @@ import java.util.Stack;
 
 public class MoteurRPN{
 	Stack<Double> p = new Stack<Double> ();
+	private final static int min_value=-999;
+	private final static int max_value=999;
 	
-	public void enregistrer(Double value) {
+
+	public MoteurRPN() 
+	{
+		p=new Stack<>();
+	}
+	
+	public void enregistrer(Double value){
+		if(min_value>value);
+		if(max_value<value);
 		p.push(value);
 	}
-	public void calcul(Operation op) {	
+	
+	public void calcul(Operation op){	
 		double resultat = 0;
-		
-		try {
-			Double v = p.pop();
-			Double y = p.pop();
+			Double v = depile();
+			Double y = depile();
 			resultat = op.eval(v, y);
 			p.push(resultat);
-		} catch (Exception e) {
-			System.out.println();
-		}
-		
-		/*if (op == Operation.ADD) {
-			Double v = p.pop();
-			Double y = p.pop();
-			resultat = Operation.ADD.eval(v, y);
-			p.push(resultat);			
-		}
-		
-		if (op == Operation.DIV) {
-			Double v = p.pop();
-			Double y = p.pop();
-			resultat = Operation.DIV.eval(v, y);
-			p.push(resultat);			
-		}
-		
-		if (op == Operation.MULT) {
-			Double v = p.pop();
-			Double y = p.pop();
-			resultat = Operation.MULT.eval(v, y);
-			p.push(resultat);			
-		}
-		
-		if (op == Operation.SUB) {
-			Double v = p.pop();
-			Double y = p.pop();
-			resultat = Operation.SUB.eval(v, y);
-			p.push(resultat);			
-		}*/
-		
-		//return;	
-		
-		}
-	
-	/*public void ListOperandes(Stack<Double> p) {
-	
-		while (p.empty()==false) {
-		
-			
-		
-			double x = p.pop();
-			p2.push(x);
-			System.out.println(x);
-		
-		}
-		
-		while(p2.empty()== false) {
-			double c = p2.pop();
-			p.push(c);
-			//System.out.println(c);
-		}*/
-		
-
-
-		
-			
-		//}
 	}
+	
+	public boolean isEmpty() 
+	{
+		return p.empty();
+		
+	}
+	
+	public double depile() 
+	{	
+		if (p.isEmpty());
+		return p.pop() ;		  
+	}
+	
+	public int nbrOperande()
+	{
+		return p.size();
+		
+	}
+	
+	public Stack<Double> getP() {
+		return p;
+	}
+	
+}
 		
 	
